faf00bd0790a91971683a84fa6430c9a3f5a643a
dghai3 <dghai3@uwo.ca>
2025-10-18 00:41:28 -0400

    Created Routes

--- Way2RareWs/frontend/src/App.jsx
+++ Way2RareWs/frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
-    <div>
+    <div className = 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] xl:px-[11vw] 2xl:px-[14vw]'>
         
+        <Routes> 
+          <Route path='/' element = {<Home/>} />
+          <Route path='/collection' element = {<Collection/>} />
+          <Route path="/about" element = {<About/>} />
+          <Route path ='/contact' element = {<Contact/>} />
+          <Route path ='/product' element = {<Product/>} />
+          <Route path ='/cart' element = {<Cart/>} />
+          <Route path ='/product/:productId' element = {<Product/>} />
+          <Route path ='/login' element = {<Login/>} />
+          <Route path ='/placeorder' element = {<PlaceOrder/>} />
+          <Route path = '/orders' element = {<Orders/>} />
+        </Routes>
     </div>
   )
 }
