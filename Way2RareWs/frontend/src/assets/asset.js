575ca702a5a13e58914c2ef91580829e22959a40
dghai3 <dghai3@uwo.ca>
2025-10-18 00:09:16 -0400

    Set up tailwindcss

---
+++ Way2RareWs/frontend/src/assets/asset.js
@@ -0,0 +1,18 @@
+import logo from "./logo.png";
+import background1 from "./background1.jpg";
+
+export const assets = {
+  logo,
+  background1,
+};
+
+export const products = {
+    _id : "0001",
+    name : "Way2Rare Zip Hoodie",
+    description : "Zip Hoodie with Way2Rare Logo",
+    price : 60,
+    image: [],
+    catergory : "Hoodies",
+    sizes : ["S", "M", "L", "XL", "XXL"],
+    current : true
+}
