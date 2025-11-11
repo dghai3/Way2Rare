55f39e11bd332d71de5fe117b37def45cf1a771d
Rayan Amir <166326023+rayanamir1@users.noreply.github.com>
2025-11-11 14:49:35 -0500

    Finished Footer
    Added links to pages, contact information to footer

--- Way2RareWs/frontend/src/components/Footer.jsx
+++ Way2RareWs/frontend/src/components/Footer.jsx
@@ -13,7 +13,31 @@ const Footer = () => {
                 </p>
             </div>
 
+            <div>
+                <p className='text-xl font-medium mb-5'>WAY2RARE</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>+1-(647)-293-8900</li>
+                    <li>waytworare@gmail.com</li>
+                </ul>
+            </div>
+
         </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2025@Way2Rare.ca - All Rights Reserved</p>
+        </div>
+
     </div>
   )
 }
